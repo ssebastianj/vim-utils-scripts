052072c03cee8d5639c1a8e57c566d7cc7679634
Sebastián J. Seba <ssebastianj@gmail.com>
2016-08-08 18:36:04 -0300

    Initial commit

---
+++ build-vim.sh
@@ -0,0 +1,14 @@
+# Compile Vim
+echo "Cleaning repo..."
+git clean -f
+echo "Garbage collection..."
+git gc
+echo "Reseting repo head..."
+git reset --hard HEAD
+echo "Pulling changes from remote..."
+git pull origin master
+
+./configure --with-features=huge --enable-multibyte --enable-cscope --enable-rubyinterp=dynamic --enable-pythoninterp=dynamic --enable-python3interp=dynamic --enable-perlinterp=dynamic --enable-luainterp --with-luajit --enable-fail-if-missing --enable-gui=auto 
+
+sudo make install
+
