af5f8fcc11d5c023a6dda72db7a72b6970116504
Sebastián J. Seba <ssebastianj@gmail.com>
2016-10-16 19:53:58 -0300

    Notable changes:
    - Make clear which Python 2/3 configuration we are using to compile vim.
    - Add script to remove vim and vim-related packages.
    - Add script to install required dependencies in Debian-like systems.

--- build-vim.sh
+++ build-vim.sh
@@ -13,7 +13,19 @@ git reset --hard HEAD
 echo "Pulling changes from remote..."
 git pull origin master
 
-./configure --with-features=huge --enable-multibyte --enable-cscope --enable-rubyinterp=dynamic --enable-pythoninterp=dynamic --enable-python3interp=dynamic --enable-perlinterp=dynamic --enable-luainterp --with-luajit --enable-fail-if-missing --enable-gui=auto 
+./configure --with-features=huge \
+            --enable-fail-if-missing \
+            --enable-multibyte \
+            --enable-cscope \
+            --enable-pythoninterp \
+            --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
+            --enable-python3interp \
+            --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu
+            --enable-rubyinterp\
+            --enable-perlinterp \
+            --enable-luainterp \
+            --with-luajit \
+            --enable-gui=auto \
 
 make clean
 sudo make install
---
+++ remove-vim.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sudo apt-get remove vim vim-runtime gvim
+sudo apt-get remove vim-tiny vim-common vim-gui-common vim-nox
+
