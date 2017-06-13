c221abbac0e8f078fb2a4dd2330b576ff5dd0d7f
Sebastián J. Seba <ssebastianj@gmail.com>
2017-06-13 20:14:04 -0300

    Compile with Python 3.6 support. Compile with 2 cores.

--- build-vim.sh
+++ build-vim.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
+# shellcheck shell=bash
+
 # Compile Vim
 
-set -e
+set -eu
 
-echo "Cleaning repo..."
+echo 'Cleaning repo...'
 git clean -f
-echo "Garbage collection..."
+make distclean
+
+echo 'Garbage collection...'
 git gc
-echo "Reseting repo head..."
+
+echo 'Reseting repo head...'
 git reset --hard HEAD
-echo "Pulling changes from remote..."
+
+echo 'Pulling changes from remote...'
 git pull origin master
 
 ./configure --with-features=huge \
@@ -18,14 +24,14 @@ git pull origin master
             --enable-multibyte \
             --enable-cscope \
             --enable-pythoninterp \
-            --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
+            --with-python-config-dir=/usr/lib/python2.7 \
             --enable-python3interp \
-            --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
-            --enable-rubyinterp\
+            --with-python3-config-dir=/usr/local/lib/python3.6 \
+            --enable-rubyinterp \
             --enable-perlinterp \
             --enable-luainterp \
             --with-luajit \
             --enable-gui=auto \
 
-make clean
+make -j2 clean
 sudo make install
