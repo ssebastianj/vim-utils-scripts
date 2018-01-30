5667ff6da98761884069780028fdd19d2079c13d
Sebastián J. Seba <ssebastianj@gmail.com>
2018-01-30 16:15:49 -0300

    Use 'apt' instead of 'apt-get'

--- install-deps-on-debian.sh
+++ install-deps-on-debian.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-set -e
+set -eu
 
-sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev \
+sudo apt install --yes libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev \
      libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev \
      libxt-dev python-dev python3-dev ruby-dev git \
      liblua5.1-dev luajit libluajit-5.1 checkinstall
