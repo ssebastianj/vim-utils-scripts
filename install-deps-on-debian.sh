99bb9c407c094630b9c5b677e3901dc32a65fa26
Sebastián J. Seba <ssebastianj@gmail.com>
2018-07-02 15:59:03 -0300

    Add Python 3.7 development header to install dependencies

--- install-deps-on-debian.sh
+++ install-deps-on-debian.sh
@@ -2,7 +2,22 @@
 
 set -eu
 
-sudo apt install --yes libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev \
-     libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev \
-     libxt-dev python-dev python3-dev ruby-dev git \
-     liblua5.1-dev luajit libluajit-5.1 checkinstall
+sudo apt install --yes \
+    libncurses5-dev \
+    libgnome2-dev \
+    libgnomeui-dev \
+    libgtk2.0-dev \
+    libatk1.0-dev \
+    libbonoboui2-dev \
+    libcairo2-dev \
+    libx11-dev \
+    libxpm-dev \
+    libxt-dev\
+    python-dev \
+    python3-dev \
+    python3.7-dev \
+    ruby-dev git \
+    liblua5.1-dev \
+    luajit \
+    libluajit-5.1 \
+    checkinstall
